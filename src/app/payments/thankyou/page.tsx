fe504dd1c8b15d7c7d08a9f0f5c9cd5d72942ebd
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-03-03 17:57:06 -0500

    Update page.tsx

--- src/app/payments/thankyou/page.tsx
+++ src/app/payments/thankyou/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Image from 'next/image';
+//import Image from 'next/image';
 import { useSession } from '../../pages/context/SessionContext';
 import Header from '../../pages/Header';
 import Footer from '../../pages/footer';
