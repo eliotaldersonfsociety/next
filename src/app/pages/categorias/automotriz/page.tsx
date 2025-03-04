10a5efceaf797d4bd3a5ec570577436e60dc2cce
eliotaldersonfsociety <aldersonelliot803@gmail.com>
2025-03-03 19:13:19 -0500

    Update page.tsx

--- src/app/pages/categorias/automotriz/page.tsx
+++ src/app/pages/categorias/automotriz/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image'; // Importar Image de next/image
 import ClipLoader from 'react-spinners/ClipLoader'; // Importar el spinner
